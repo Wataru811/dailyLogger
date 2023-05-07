181a3176feccddcff4b318eb3a1ac399de7a2dec
Jaichael Ehrmantraut <wash.fuel@gmail.com>
2023-05-07 17:06:31 +0700

    v1.0.3, builder.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 
-npx electron-builder --mac
-npx electron-builder --dir --mac --universal
-npx electron-forge package --arch=x64 --platform=darwin
-npx electron-froge package --arch=universal --platform=darwin
+npx electron-builder --mac 
+npx electron-builder --win --x64
+npx electron-builder --linux --x64
+#npx electron-builder --dir --mac --universal
+#npx electron-forge package --arch=x64 --platform=darwin
+#npx electron-froge package --arch=universal --platform=darwin
